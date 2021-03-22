423a95635e432bbbf41ca416d2569a6a3f644a05
Grace Hawkins <amoomajid99@gmail.com>
2021-03-22 06:56:42 +0430

    Improve tester.sh script to autodetect the tests

--- tester.sh
+++ tester.sh
@@ -4,16 +4,21 @@ function clean_files {
     rm -f input.txt lexical_errors.txt symbol_table.txt tokens.txt
 }
 
+if [ $# -ne 1 ]
+then
+    echo pass the tests root addrees
+    exit 1
+fi
+
 samples_dir="$1"
 
-for i in {1..10}
+for test_dir in "$samples_dir"/T*
 do
     clean_files
 
-    test_name=T`printf "%02d" $i`
-    test_dir="$samples_dir/$test_name"
+    test_name=`basename $test_dir`
 
-    echo Test \#$i
+    echo Test $test_name
     ./single-test.sh $test_dir
     echo
 done
